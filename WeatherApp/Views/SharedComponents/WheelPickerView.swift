50305e4a28a94a071eefdb2d6a6468cee8d8543e
asheshp23 <asheshp23@gmail.com>
2024-05-09 13:58:04 -0400

    Minor chagges

---
+++ WeatherApp/Views/SharedComponents/WheelPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  WheelPickerView.swift
+//  WeatherApp
+//
+//  Created by Ashesh Patel on 2024-04-21.
+//
+
+import SwiftUI
+
+struct WheelPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WheelPickerView()
+}
